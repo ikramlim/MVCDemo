c79f3ed5ec0fa13afac74c4093966733dcbe6f9d
Ikram_Lim <ikramlim@gmail.com>
2015-12-23 01:00:07 +0800

    AjaxJquery
    AjaxJquery

---
+++ MVCDemo/Controllers/DemoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HelloAjax()
+        {
+            return Content("Hello ikramlim", "text/plain");
+        }
+        public ActionResult Sum(int a, int b)
+        {
+            return Content((a + b).ToString(), "text/plain");
+        }
+
+        public ActionResult DisplayObject()
+        {
+            Product p1 = new Product("pr1", "Name 1", 1000);
+            return Json(p1, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult DisplayListObject()
+        {
+            List<Product> listProduct = new List<Product>();
+            listProduct.Add(new Product("pr1", "Name 1", 1000));
+            listProduct.Add(new Product("pr2", "Name 2", 2000));
+            listProduct.Add(new Product("pr3", "Name 3", 3000));
+            return Json(listProduct, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCDemo/Models/Product.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCDemo.Models
+{
+    public class Product
+    {
+        private string id;
+        private string name;
+        private decimal price;
+
+        public Product() { }
+
+        public Product(string id, string name, decimal price)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+    }
+}
